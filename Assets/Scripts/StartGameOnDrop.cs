aef21c09452f108323ddd6e7eba92aa7c0323a65
Toby-Scantlebury <toby.scantlebury@advancednavigation.com.au>
2019-04-21 20:25:40 +0800

    Happy cat on menu when fed

--- Assets/Scripts/StartGameOnDrop.cs
+++ Assets/Scripts/StartGameOnDrop.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGameOnDrop : MonoBehaviour
 {
+    [SerializeField]
+    Sprite eatingSprite;
+
     void OnDrop(GameObject droppedObject)
     {
+        GetComponent<Image>().sprite = eatingSprite;
         GameObject.Find("GameManager").GetComponent<FadeManager>().StartFade(() => SceneManager.LoadScene("MainScene"));
     }
 }
