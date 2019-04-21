2af7978f1db14f7f4875caee9ea6de87bdaf4d2c
sam6321 <sam6321@live.com.au>
2019-04-21 14:02:24 +0800

    Set up food spawning

--- Assets/Scripts/FoodManager.cs
+++ Assets/Scripts/FoodManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,6 +10,11 @@ public class FoodManager : MonoBehaviour
 
     [SerializeField]
     private CatGod catGod;
+
+    [SerializeField]
+    private GameObject spawnVolume;
+
+    Coroutine spawnFoodCoroutine;
 
     private GameObject[] foodObjects;
     private Food[] foods;
@@ -27,16 +33,13 @@ public class FoodManager : MonoBehaviour
     {
         Food[] shuffledFoods = Utils.Shuffle((Food[])foods.Clone());
         List<Food> selectedFoods = shuffledFoods.Take(count).ToList();
-        foreach(Food food in selectedFoods)
-        {
-            food.gameObject.SetActive(true);
-        }
+        spawnFoodCoroutine = StartCoroutine(SpawnFood(selectedFoods));
         return selectedFoods;
     }
 
-    public void CleanupFoods(List<Food> foods)
+    public void CleanupFoods(List<Food> cleanup)
     {
-        foreach(Food food in foods)
+        foreach(Food food in cleanup)
         {
             food.gameObject.SetActive(false);
         }
@@ -45,5 +48,20 @@ public class FoodManager : MonoBehaviour
     public int MaxFood
     {
         get { return foodObjects.Length; }
+    }
+
+    IEnumerator SpawnFood(List<Food> selectedFoods)
+    {
+        int spawned = 0;
+        Vector2 spawnVolumeOffset = new Vector2(spawnVolume.transform.localScale.x * 0.5f, 0.0f);
+        Vector2 spawnStart = (Vector2)spawnVolume.transform.position - spawnVolumeOffset;
+        Vector2 spawnEnd = (Vector2)spawnVolume.transform.position + spawnVolumeOffset;
+        foreach (Food food in selectedFoods)
+        {
+            food.gameObject.transform.position = Vector2.Lerp(spawnStart, spawnEnd, Mathf.PingPong(spawned, 3) / 3.0f);
+            food.gameObject.SetActive(true);
+            spawned++;
+            yield return new WaitForSeconds(0.15f);
+        }
     }
 }
