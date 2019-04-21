10bceee6905376ecfcd997c5d465ec0e9370b118
Toby-Scantlebury <toby.scantlebury@advancednavigation.com.au>
2019-04-21 10:46:47 +0800

    Add colision audio for lantern

---
+++ Assets/Scripts/CollisionAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAudio : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> allAudio;
+
+     void OnCollisionEnter2D ()
+     {
+        GetComponent<AudioSource>().clip = allAudio[UnityEngine.Random.Range(0, allAudio.Count)];
+        GetComponent<AudioSource>().Play();
+     }
+}
