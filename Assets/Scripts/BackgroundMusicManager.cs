3fcb1882eb2cb05926107e1d70964cfc0aa5c7c4
Toby-Scantlebury <toby.scantlebury@advancednavigation.com.au>
2019-04-22 10:22:21 +0800

    Cut background music on lose

---
+++ Assets/Scripts/BackgroundMusicManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnFavourHitZero()
+    {
+        audioSource.Stop();
+    }
+}
