d365274230567f4d5c44bf2530557fe91118fcb9
sam6321 <sam6321@live.com.au>
2019-04-22 09:40:09 +0800

    Added food sound on feeding the main menu cat

--- Assets/Scripts/StartGameOnDrop.cs
+++ Assets/Scripts/StartGameOnDrop.cs
@@ -9,9 +9,13 @@ public class StartGameOnDrop : MonoBehaviour
     [SerializeField]
     Sprite eatingSprite;
 
+    [SerializeField]
+    private AudioClip[] onDropSounds;
+
     void OnDrop(GameObject droppedObject)
     {
         GetComponent<Image>().sprite = eatingSprite;
+        Utils.PlayRandomSound(GetComponent<AudioSource>(), onDropSounds);
         GameObject.Find("GameManager").GetComponent<FadeManager>().StartFade(() => SceneManager.LoadScene("MainScene"));
     }
 }
